fb9912ac8d7cc44947c7fab48a364f42bd7115da
xiao <68981038+bbbkkp@users.noreply.github.com>
2022-07-20 17:48:10 +0800

    Update org-tree.vue
    测试是否可以执行到这里

--- src/components/org-tree/org-tree.vue
+++ src/components/org-tree/org-tree.vue
@@ -68,6 +68,7 @@ export default {
         const { expand } = this.flatData[item[this.prop_id]] || {}
         if (expand) this.$set(item, this.prop_expand, true)
       })
+      console.error("!!!!!!!!----xiaobing------!!!!!!!!!");
       //this._toggleExpand(this.dataCloned, this.expandAll)
     },
     expandAll (status) {
